faf81595a53d4e0ea969b12e5aa4b56e1db4a961
appu <bajarlaapoorva@gmail.com>
2023-11-08 20:36:22 +0530

    company

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React,{useEffect, useState} from "react"
-import axios from "axios"
-function FetchData() {
-  const[data, setData] = useState([])
-  useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(res=>setData(res.data))
-    .catch(err=>console.log(err));
-  },[])
-  return(
-    <div>
-    {
-    data.map((user,index) => {
-      return<ol key={index}>
-        <li>{user.name}</li>
-      </ol>
-    }) 
-  }
-   </div> 
-  
-  )}
-export default FetchData
+import React,{useState} from "react";
+import "./App.css"
+import {Header}from "./Header"
+import{Company}from"./Company"
+import { CompanyCard } from "./Companycard"
+
+
+const App= ()=>{
+    const [CompanyList,updateCompanyList]=useState([
+{companyUrl:"https://tse3.mm.bing.net/th?id=OIP.XWM7kToC-J76ys2lXsALPgHaE7&pid=Api&P=0&h=180",
+companyName:"Tata Consultancy",id:"1"},
+{companyUrl:"https://tse3.mm.bing.net/th?id=OIP.zj65TQ6bpp-HZml30RWtUAHaE8&pid=Api&P=0&h=180",
+companyName:"Infosys",id:"2"},
+{companyUrl:"https://tse4.mm.bing.net/th?id=OIP.TF_K3fz6YPpJwyOeGbwHewHaEm&pid=Api&P=0&h=180",
+companyName:"HCL Technologie",id:"3"},
+{companyUrl:"https://tse1.mm.bing.net/th?id=OIP.1y4Teu95O7aMN9OvOFhp9wHaFj&pid=Api&P=0&h=180",
+companyName:"Wipro",id:"4"},
+{companyUrl:"https://tse3.mm.bing.net/th?id=OIP.FmvwLVTdkBA7oyNRMYMM1gHaEc&pid=Api&P=0&h=180",
+companyName:"Tech Mahindra",id:"5"}
+])
+return(
+    <>
+    
+    <Header/>
+    <Company/>
+    {CompanyList.map( (item,i)=>{
+        console.log(item)
+return<CompanyCard Name={item.companyName}Img={item.companyUrl} Link={item.id} />
+    })}
+    </>
+)
+}
+export default App;
---
+++ src/Company.js
@@ -0,0 +1,20 @@
+export const Company=()=>{
+    return(
+        <>
+        <div className="container-fluid ">
+      <div className="row ">
+      <div className="col-12 pt-4 ">
+        <h1>
+            Top <span>5 Software</span> Companies
+        </h1></div>
+        <div className="col ">
+        <ul>
+          <a className="text-dark text-decoration-none"href="./index.html#2"><li><h4>Tata Consultancy</h4></li></a>
+          <a className="text-dark text-decoration-none"href="./index.html#3"><li><h4>Infosys</h4></li></a>
+          <a className="text-dark text-decoration-none"href="./index.html#4"><li><h4>HCL Technologie</h4></li></a>
+          <a className="text-dark text-decoration-none"href="./index.html#5"><li><h4>Wipro</h4></li></a>
+          <a className="text-dark text-decoration-none"href="./index.html#2"><li><h4>Tech Mahindra</h4></li></a>
+        </ul></div></div></div>
+        </>
+    )
+    }
